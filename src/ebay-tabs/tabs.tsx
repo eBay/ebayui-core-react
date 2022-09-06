613575f6c30b13e269e734d6f7b1dcc9b93104cb
josou <josou@ebay.com>
2022-09-06 15:14:48 -0700

    Fix tab header stealing focus from inputs

--- src/ebay-tabs/tabs.tsx
+++ src/ebay-tabs/tabs.tsx
@@ -42,8 +42,8 @@ class Tabs extends Component<TabsProps, State> {
     componentDidUpdate(prevProps: TabsProps): void {
         if (this.props.index !== prevProps.index) {
             this.onTabSelect(this.props.index)
+            this.headings[this.state.focusedIndex]?.focus()
         }
-        this.headings[this.state.focusedIndex]?.focus()
     }
 
     onTabSelect(i: number): void {
