09c2efa6298ef676a0c64f7d2eea835573576714
Henrique Limas <henrique.ramos.limas@gmail.com>
2022-10-07 10:49:27 -0700

    EbaySectionTitle: Simplified html structure (#106)
    * EbaySectionTitle: Simplified html structure
    
    * Remove size attribute
    
    Co-authored-by: hlimas <hlimas@ebay.com>
    Co-authored-by: Timur Manyanov <2737310+darkwebdev@users.noreply.github.com>

--- src/ebay-section-title/cta.tsx
+++ src/ebay-section-title/cta.tsx
@@ -1,37 +1,25 @@
 import React, { ComponentProps, FC, ReactNode } from 'react'
 import classNames from 'classnames'
-import { EbayIcon, Icon } from '../ebay-icon'
 
-type Props = ComponentProps<'div'> & {
+type Props = ComponentProps<'a'> & {
   ctaText?: ReactNode;
   href?: string;
-  icon?: Icon;
 }
 
 const Cta: FC<Props> = ({
     ctaText,
     href,
-    icon = 'arrowRightBold',
+    className,
     ...rest
 }: Props) => {
-    const className = classNames('section-title__cta', {
-        'section-title__cta--no-text': !ctaText
-    })
+    if (!ctaText) {
+        return null
+    }
 
     return (
-        <div {...rest} className={className}>
-            <a href={href} tabIndex={-1} aria-hidden="true">
-                {ctaText && <span className="section-title__cta-text">{ctaText}</span>}
-                <EbayIcon
-                    name={icon}
-                    className="section-title__cta-icon"
-                    noSkinClasses
-                    height="24"
-                    width="24"
-                    aria-hidden="true"
-                />
-            </a>
-        </div>
+        <a {...rest} className={classNames('section-title__cta', className)} href={href}>
+            {ctaText}
+        </a>
     )
 }
 
