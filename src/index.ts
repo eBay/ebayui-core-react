5d9402c694ebfade42af672803c92c8b4d8f2d60
Andrew Cho <ancho@ebay.com>
2022-11-10 06:23:43 -0800

    merge

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,7 @@ export { EbayCtaButton } from './ebay-cta-button'
 export { EbayEek, EbayEekProps } from './ebay-eek'
 export { EbayIconButton, EbayIconButtonProps } from './ebay-icon-button'
 export { EbayMenu, EbayMenuItem, EbayMenuSeparator, EbayMenuType, EbayMenuPriority } from './ebay-menu'
-export { EbayMenuButton, EbayMenuButtonLabel } from './ebay-menu-button'
 export { EbayFakeMenu, EbayFakeMenuItem, EbayFakeMenuSeparator, EbayFakeMenuItemProps } from './ebay-fake-menu'
-export {
-    EbayFakeMenuButton, EbayFakeMenuButtonItem, EbayFakeMenuButtonSeparator, EbayFakeMenuButtonLabel
-} from './ebay-fake-menu-button'
 export { EbayInlineNotice, NoticeStatus } from './ebay-inline-notice'
 export { EbaySectionNotice, EbaySectionNoticeTitle, EbaySectionNoticeFooter } from './ebay-section-notice'
 export {
