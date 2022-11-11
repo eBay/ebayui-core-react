bed27e7ea4fddf3c5d233afbe93de9ce75ddbdc5
Timur Manyanov <2737310+darkwebdev@users.noreply.github.com>
2022-11-11 19:28:43 +0100

    Add tests

--- src/ebay-fake-menu/__tests__/index.spec.tsx
+++ src/ebay-fake-menu/__tests__/index.spec.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { createRef } from 'react'
 import { fireEvent, render } from '@testing-library/react';
 import initStoryshots from '@storybook/addon-storyshots'
 import { EbayFakeMenu, EbayFakeMenuItem } from '../index'
@@ -19,7 +19,7 @@ describe('<EbayFakeMenu>', () => {
 
             fireEvent.click(wrapper.container.querySelectorAll('.fake-menu__item')[0])
 
-            expect(onClickSpy).toBeCalled()            
+            expect(onClickSpy).toBeCalled()
         })
     })
     describe('on key down', () => {
@@ -49,6 +49,32 @@ describe('<EbayFakeMenu>', () => {
             fireEvent.click(wrapper.container.querySelectorAll('.fake-menu__item')[1])
 
             expect(onSelectSpy).toBeCalledWith(expect.anything(), 1)
+        })
+    })
+
+    describe('on appear', () => {
+        it('should not focus on first item given no ref', () => {
+            const wrapper = render(
+                <EbayFakeMenu>
+                    <EbayFakeMenuItem href="#" />
+                    <EbayFakeMenuItem href="#" />
+                </EbayFakeMenu>
+            )
+            const firstMenuItem = wrapper.container.querySelectorAll('.fake-menu__item')[0]
+
+            expect(firstMenuItem).not.toHaveFocus()
+        })
+        it('should focus on first item given ref', () => {
+            const ref: any = createRef()
+            const wrapper = render(
+                <EbayFakeMenu>
+                    <EbayFakeMenuItem href="#" itemRef={ref} />
+                    <EbayFakeMenuItem href="#" />
+                </EbayFakeMenu>
+            )
+            const firstMenuItem = wrapper.container.querySelectorAll('.fake-menu__item')[0]
+
+            expect(firstMenuItem).toHaveFocus()
         })
     })
 })
