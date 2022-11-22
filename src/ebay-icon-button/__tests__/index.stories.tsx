ea88c6d21d5ae3df60412205bff5e5170fb66ee5
Timur Manyanov <2737310+darkwebdev@users.noreply.github.com>
2022-11-22 13:34:37 +0100

    EbayIconButton: Update stories with a11y examples and readme

--- src/ebay-icon-button/__tests__/index.stories.tsx
+++ src/ebay-icon-button/__tests__/index.stories.tsx
@@ -6,19 +6,42 @@ import { EbayIconButton } from '../../index'
 storiesOf(`ebay-icon-button`, module)
     .add(`Default`, () => (
         <>
-            <p><EbayIconButton onClick={action(`clicked`)} icon="menu" /></p>
-            <p><EbayIconButton href="https://ebay.com" icon="settings" /></p>
+            <p><EbayIconButton onClick={action(`clicked`)} icon="menu" aria-label="menu" /></p>
+            <p><EbayIconButton href="https://ebay.com" icon="settings" aria-label="settings" /></p>
         </>
     ))
     .add(`With Badges`, () => (
         <>
-            <p><EbayIconButton icon="menu" badgeNumber={1} badgeAriaLabel="new feature available"/></p>
-            <p><EbayIconButton href="https://ebay.com" icon="cart" badgeNumber={3} badgeAriaLabel="3 items in your cart"/></p>
-            <p><EbayIconButton href="https://ebay.com" icon="chatLarge" badgeNumber={99} badgeAriaLabel="99 unread messages"/></p>
+            <p>
+                <EbayIconButton
+                    icon="menu"
+                    aria-label="menu"
+                    badgeNumber={1}
+                    badgeAriaLabel="new feature available"
+                />
+            </p>
+            <p>
+                <EbayIconButton
+                    href="https://ebay.com"
+                    icon="cart"
+                    aria-label="cart"
+                    badgeNumber={3}
+                    badgeAriaLabel="3 items in your cart"
+                />
+            </p>
+            <p>
+                <EbayIconButton
+                    href="https://ebay.com"
+                    icon="chatLarge"
+                    aria-label="chat"
+                    badgeNumber={99}
+                    badgeAriaLabel="99 unread messages"
+                />
+            </p>
         </>
     ))
     .add(`Transparent`, () => (
         <>
-            <p><EbayIconButton onClick={action(`clicked`)} icon="menu" transparent /></p>
+            <p><EbayIconButton onClick={action(`clicked`)} icon="menu" transparent aria-label="menu" /></p>
         </>
     ))
