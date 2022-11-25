93804629ca7c0e34efd15e47639850f642ff58a7
Timur Manyanov <2737310+darkwebdev@users.noreply.github.com>
2022-11-25 12:40:21 +0100

    EbayButton: add `split` prop (#166)

--- src/ebay-button/button-expand.tsx
+++ src/ebay-button/button-expand.tsx
@@ -3,13 +3,16 @@ import EbayButtonCell from './button-cell'
 import EbayButtonText from './button-text'
 import { EbayIcon } from '../ebay-icon'
 
-const EbayButtonExpand: FC = ({ children }) => (
-    <EbayButtonCell>
-        <EbayButtonText>
-            {children}
-        </EbayButtonText>
+const EbayButtonExpand: FC = ({ children }) =>
+    children ? (
+        <EbayButtonCell>
+            <EbayButtonText>
+                {children}
+            </EbayButtonText>
+            <EbayIcon name="dropdown" />
+        </EbayButtonCell>
+    ) : (
         <EbayIcon name="dropdown" />
-    </EbayButtonCell>
-)
+    )
 
 export default EbayButtonExpand
