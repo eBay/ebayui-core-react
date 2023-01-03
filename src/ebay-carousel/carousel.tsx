74414c636d5bd48cd48205289ff42d9a4188e420
Flakron Kadriu <fkadriu@ebay.com>
2023-01-03 16:24:38 +0100

    added comments

--- src/ebay-carousel/carousel.tsx
+++ src/ebay-carousel/carousel.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unused-prop-types */
 import React, { FC, ReactNode } from 'react'
 import classNames from 'classnames'
 import CarouselControlButton from './carousel-control-button'
@@ -7,20 +8,40 @@ type CarouselProps = {
     className?: string;
     children?: ReactNode;
     gap?: number;
+    index?: number; // Scroll to index slide
+    imageTreatment?: boolean; // Image slide?
+    itemsPerSlide?: number; // number of slides to be displayed
+    previousText?: string;
+    nextText?: string;
+    label?: string;
+    onCarouselUpdate: () => void;
+    onNext: () => void;
+    onPrevious: () => void;
+    onScroll: () => void;
+    onPlay: (event: React.SyntheticEvent) => void
+    onPause: (event: React.SyntheticEvent) => void
 };
+
+// TO-DO:
+// Scroll through slides using prev,next buttons
+// Image slides
+// Auto play
 
 const EbayCarousel: FC<CarouselProps> = ({
     gap,
+    label,
+    previousText,
+    nextText,
     className,
     children
 }) => (
-    <div className={classNames('carousel', className)}>
+    <div className={classNames('carousel', className)} aria-label={label}>
         <div className="carousel__container">
-            <CarouselControlButton type="prev" label="Previous Slide" />
+            <CarouselControlButton label={previousText} type="prev" />
             <CarouselList gap={gap}>
                 {children}
             </CarouselList>
-            <CarouselControlButton type="next" label="Next Slide" />
+            <CarouselControlButton type="next" label={nextText} />
         </div>
     </div>
 )
