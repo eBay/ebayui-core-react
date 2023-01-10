67427f076ec19f5f711064607247480fe6689eca
Henrique Limas <henrique.ramos.limas@gmail.com>
2023-01-10 13:03:13 -0800

    FIX: Update latest tooltip styles definition
    Fixes #192

--- src/common/tooltip-utils/constants.ts
+++ src/common/tooltip-utils/constants.ts
@@ -3,87 +3,85 @@ import { PointerDirection } from './types'
 export const DEFAULT_POINTER_DIRECTION: PointerDirection = 'bottom'
 
 export const POINTER_STYLES: { [key in PointerDirection]: any } = {
-    left: {
-        transform: 'translateX(16px) translateY(-50%)',
+    'left': {
+        transform: 'translateX(16px) translateY(-50%) scale3d(1,1,1)',
         left: '100%',
         right: 'auto',
-        top: '-5px',
+        top: '-6px',
         bottom: 'auto'
     },
-
     'left-top': {
-        transform: 'translateX(16px)',
+        transform: 'translateX(16px) scale3d(1,1,1)',
         left: '100%',
         right: 'auto',
         top: '-100%',
         bottom: 'auto'
     },
-
     'left-bottom': {
-        transform: 'translateX(16px)',
+        transform: 'translateX(16px) scale3d(1,1,1)',
         left: '100%',
         right: 'auto',
         top: 'auto',
-        bottom: '-7px'
+        bottom: '-10px'
     },
-
     'right': {
-        transform: 'translateX(-16px) translateY(-50%)',
+        transform: 'translateX(-16px) translateY(-50%) scale3d(1,1,1)',
         left: 'auto',
         right: '100%',
-        top: '-4px',
+        top: '-6px',
         bottom: 'auto'
     },
-
     'right-top': {
-        transform: 'translateX(-16px)',
+        transform: 'translateX(-16px) scale3d(1,1,1)',
         left: 'auto',
         right: '100%',
         top: '-100%',
         bottom: 'auto'
     },
-
     'right-bottom': {
-        transform: 'translateX(-16px)',
+        transform: 'translateX(-16px) scale3d(1,1,1)',
         left: 'auto',
         right: '100%',
         top: 'auto',
         bottom: '-50%'
     },
-
     'top': {
-        transform: 'translateX(-50%)',
+        transform: 'translateX(-50%) scale3d(1,1,1)',
         left: '50%',
         right: 'auto',
-        top: 'auto',
+        top: 'calc(100% + 2px)',
         bottom: 'auto'
     },
-
-    'top-left': {},
-
+    'top-left': {
+        transform: 'scale3d(1,1,1)',
+        left: '-10px',
+        right: 'auto',
+        top: 'calc(100% + 2px)',
+        bottom: 'auto'
+    },
     'top-right': {
+        transform: 'scale3d(1,1,1)',
         left: 'auto',
-        right: '-7px',
-        top: 'auto',
+        right: '-10px',
+        top: 'calc(100% + 2px)',
         bottom: 'auto'
     },
-
     'bottom-right': {
+        transform: 'scale3d(1,1,1)',
         left: 'auto',
-        right: '-7px',
+        right: '-10px',
         top: 'auto',
         bottom: 'calc(100% + 12px)'
     },
-
     'bottom-left': {
-        left: '-7px',
+        transform: 'scale3d(1,1,1)',
+        left: '-10px',
         right: 'auto',
         top: 'auto',
         bottom: 'calc(100% + 12px)'
     },
-
     'bottom': {
-        transform: 'translateX(-50%)',
+        transform: 'translateX(-50%) scale3d(1,1,1)',
         left: '50%',
         right: 'auto',
         top: 'auto',
