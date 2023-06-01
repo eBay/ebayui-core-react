5c4ef548dd9ac6c79d4f5dd43239010c6e1fe6c9
spandian <spandian@ebay.com>
2023-06-01 16:31:24 -0700

    removing it from common util as wellr

--- src/common/event-utils/types.ts
+++ src/common/event-utils/types.ts
@@ -1,6 +1,3 @@
-import { ChangeEvent, KeyboardEvent, MouseEvent, FocusEvent, SyntheticEvent } from 'react'
-
-
 type ModifierKeys = 'Alt' | 'AltGraph' | 'Control' | 'Shift' | 'CapsLock' | 'Meta'
 // | 'Fn' | 'FnLock' | 'Hyper' | 'NumLock' | 'ScrollLock' | 'Super' | 'Symbol' | 'SymbolLock'
 type NavigationKeys = 'ArrowDown' | 'ArrowLeft' | 'ArrowRight' | 'ArrowUp' | 'Enter' | 'Tab' | ' ' | 'Escape'
@@ -13,15 +10,3 @@ type NavigationKeysEdge = 'Down' | 'Left' | 'Right' | 'Up' | 'Esc'
 
 export type Key = ModifierKeys | NavigationKeys | NavigationKeysEdge
 
-type BaseEventHandler<E extends SyntheticEvent<any>, P> = (event: E, props?: P) => void;
-
-export type EbayEventHandler<ElementType = Element, PropsObject = Record<string, any>> =
-    BaseEventHandler<SyntheticEvent<ElementType>, PropsObject>;
-export type EbayMouseEventHandler<ElementType = Element, PropsObject = Record<string, any>> =
-    BaseEventHandler<MouseEvent<ElementType>, PropsObject>;
-export type EbayKeyboardEventHandler<ElementType = Element, PropsObject = Record<string, any>> =
-    BaseEventHandler<KeyboardEvent<ElementType>, PropsObject>;
-export type EbayChangeEventHandler<ElementType = Element, PropsObject = Record<string, any>> =
-    BaseEventHandler<ChangeEvent<ElementType>, PropsObject>;
-export type EbayFocusEventHandler<ElementType = Element, PropsObject = Record<string, any>> =
-    BaseEventHandler<FocusEvent<ElementType>, PropsObject>;
