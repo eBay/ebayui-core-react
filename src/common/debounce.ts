747c4bed605715021ad8be0797cec35c7ea35135
Timur Manyanov <2737310+darkwebdev@users.noreply.github.com>
2023-11-15 17:13:05 +0100

    [#285] TS: strict:true, noImplicitThis: true

--- src/common/debounce.ts
+++ src/common/debounce.ts
@@ -1,10 +1,9 @@
-export function debounce(fn: (any) => any, ms: number): () => void {
-    let timer
-    return () => {
-        clearTimeout(timer)
-        timer = setTimeout((...args) => {
-            timer = null
-            fn.apply(this, ...args)
-        }, ms)
-    }
-}
+export const debounce =
+  <F extends (...args: Parameters<F>) => ReturnType<F>>(fn: F, ms: number): (...args: Parameters<F>) => void => {
+      let timer: ReturnType<typeof setTimeout>
+
+      return (...args: Parameters<F>) => {
+          clearTimeout(timer)
+          timer = setTimeout(() => fn(...args), ms)
+      }
+  }
