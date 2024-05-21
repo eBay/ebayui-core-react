1a227e124db46d6b7a84ace6d29d03ff6b6b51cb
Caleb Nance <calebnance@gmail.com>
2024-05-21 15:44:16 -0400

    updated button mappings

--- src/ebay-button/index.figma.tsx
+++ src/ebay-button/index.figma.tsx
@@ -3,6 +3,7 @@ import figma from '@figma/code-connect'
 import React from 'react'
 import { EbayButton } from '.'
 
+// button
 figma.connect(
     EbayButton,
     'https://www.figma.com/design/zEBdEhbonrBOGzZ0fXzWvM/eBay-Design-System?node-id=65849-91206&m=dev',
@@ -20,7 +21,74 @@ figma.connect(
             })
         },
         example: ({ state, size, title }) => (
-            <EbayButton disabled={state} size={size}>
+            <EbayButton disabled={state} size={size} variant="standard">
+                {title}
+            </EbayButton>
+        )
+    }
+)
+
+// button destructive
+figma.connect(
+    EbayButton,
+    'https://www.figma.com/design/zEBdEhbonrBOGzZ0fXzWvM/eBay-Design-System?node-id=129298-738861&m=dev',
+    {
+        props: {
+            state: figma.enum('state', {
+                disabled: true,
+                enabled: false
+            }),
+            title: figma.string('Title'),
+            size: figma.enum('size', {
+                medium: 'regular',
+                small: 'small',
+                large: 'large'
+            })
+        },
+        example: ({ state, size, title }) => (
+            <EbayButton disabled={state} size={size} variant="destructive">
+                {title}
+            </EbayButton>
+        )
+    }
+)
+
+// button branded
+figma.connect(
+    EbayButton,
+    'https://www.figma.com/design/zEBdEhbonrBOGzZ0fXzWvM/eBay-Design-System?node-id=129776-224141&m=dev',
+    {
+        props: {
+            title: figma.string('Title'),
+            size: figma.enum('size', {
+                medium: 'regular',
+                small: 'small',
+                large: 'large'
+            })
+        },
+        example: ({ size, title }) => (
+            <EbayButton size={size}>
+                {title}
+            </EbayButton>
+        )
+    }
+)
+
+// button link (borderless)
+figma.connect(
+    EbayButton,
+    'https://www.figma.com/design/zEBdEhbonrBOGzZ0fXzWvM/eBay-Design-System?node-id=109500-131885&m=dev',
+    {
+        props: {
+            title: figma.string('Title'),
+            size: figma.enum('size', {
+                medium: 'regular',
+                small: 'small',
+                large: 'large'
+            })
+        },
+        example: ({ size, title }) => (
+            <EbayButton size={size} borderless>
                 {title}
             </EbayButton>
         )
---
+++ src/ebay-icon-button/index.figma.tsx
@@ -0,0 +1,15 @@
+import figma from '@figma/code-connect'
+
+import React from 'react'
+import { EbayIconButton } from '.'
+
+// button
+figma.connect(
+    EbayIconButton,
+    'https://www.figma.com/design/zEBdEhbonrBOGzZ0fXzWvM/eBay-Design-System?node-id=65935-91936&m=dev',
+    {
+        example: () => (
+            <EbayIconButton icon="settings16" aria-label="settings" />
+        )
+    }
+)
