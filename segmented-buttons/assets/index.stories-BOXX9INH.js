a27bedc3797b3a3ce821acfb05bdf3fb3a2e054a
tmanyanov <2737310+darkwebdev@users.noreply.github.com>
2024-10-10 17:53:37 -0700

    Deploying to gh-pages from @ eBay/ebayui-core-react@afc728f 🚀

---
+++ segmented-buttons/assets/index.stories-BOXX9INH.js
@@ -0,0 +1,22 @@
+import{j as y,F as i,a as e}from"./jsx-runtime-CjohWU4o.js";import{a as r}from"./chunk-AY7I2SME-DUSEA8To.js";import{E as n}from"./icon-button-BahlHWhV.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-Bl6ORisp.js";import"./icon-DvI5HAVZ.js";import"./array.polyfill.flat--BFOj5KS.js";import"./badge-D7xwVPWb.js";const A={title:"buttons/ebay-icon-button"},o=()=>y(i,{children:[e("p",{children:e(n,{onClick:a=>r("onClick")(a),onFocus:a=>r("onFocus")(a),onBlur:a=>r("onBlur")(a),onEscape:a=>r("onEscape")(a),icon:"menu20","aria-label":"Menu"})}),e("p",{children:e(n,{href:"https://ebay.com",icon:"settings16","aria-label":"Settings"})})]}),t=()=>y(i,{children:[e("p",{children:e(n,{icon:"menu20","aria-label":"Menu",badgeNumber:1,badgeAriaLabel:"new feature available"})}),e("p",{children:e(n,{href:"https://ebay.com",icon:"cart16",badgeNumber:3,badgeAriaLabel:"3 items in your cart","aria-label":"Cart"})}),e("p",{children:e(n,{href:"https://ebay.com",icon:"chat16",badgeNumber:99,badgeAriaLabel:"99 unread messages","aria-label":"Chat"})})]}),c=()=>e(i,{children:e("p",{children:e(n,{onClick:r("clicked"),icon:"menu20",transparent:!0,"aria-label":"Menu"})})});var s,l,u;o.parameters={...o.parameters,docs:{...(s=o.parameters)==null?void 0:s.docs,source:{originalSource:`() => <>
+        <p>
+            <EbayIconButton onClick={(e: React.MouseEvent) => action('onClick')(e)} onFocus={(e: React.FocusEvent) => action('onFocus')(e)} onBlur={(e: React.FocusEvent) => action('onBlur')(e)} onEscape={(e: React.KeyboardEvent) => action('onEscape')(e)} icon="menu20" aria-label="Menu" />
+        </p>
+        <p>
+            <EbayIconButton href="https://ebay.com" icon="settings16" aria-label="Settings" />
+        </p>
+    </>`,...(u=(l=o.parameters)==null?void 0:l.docs)==null?void 0:u.source}}};var b,p,m;t.parameters={...t.parameters,docs:{...(b=t.parameters)==null?void 0:b.docs,source:{originalSource:`() => <>
+        <p>
+            <EbayIconButton icon="menu20" aria-label="Menu" badgeNumber={1} badgeAriaLabel="new feature available" />
+        </p>
+        <p>
+            <EbayIconButton href="https://ebay.com" icon="cart16" badgeNumber={3} badgeAriaLabel="3 items in your cart" aria-label="Cart" />
+        </p>
+        <p>
+            <EbayIconButton href="https://ebay.com" icon="chat16" badgeNumber={99} badgeAriaLabel="99 unread messages" aria-label="Chat" />
+        </p>
+    </>`,...(m=(p=t.parameters)==null?void 0:p.docs)==null?void 0:m.source}}};var d,h,g;c.parameters={...c.parameters,docs:{...(d=c.parameters)==null?void 0:d.docs,source:{originalSource:`() => <>
+        <p>
+            <EbayIconButton onClick={action('clicked')} icon="menu20" transparent aria-label="Menu" />
+        </p>
+    </>`,...(g=(h=c.parameters)==null?void 0:h.docs)==null?void 0:g.source}}};const L=["Default","WithBadges","Transparent"];export{o as Default,c as Transparent,t as WithBadges,L as __namedExportsOrder,A as default};
