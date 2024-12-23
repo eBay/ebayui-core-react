d4adc7d455635a880746c6a9615324992078cf88
Bruno Kern <bruduarte@ebay.com>
2024-12-23 13:49:18 -0300

    get initial state without useEffect hook

--- src/ebay-toggle-button-group/toggle-button-group.tsx
+++ src/ebay-toggle-button-group/toggle-button-group.tsx
@@ -1,10 +1,4 @@
-import React, {
-    ReactElement,
-    cloneElement,
-    useState,
-    useCallback,
-    useEffect
-} from 'react'
+import React, { ReactElement, cloneElement, useState, useCallback } from 'react'
 import cx from 'classnames'
 import { filterByType } from '../common/component-utils'
 import {
@@ -31,11 +25,20 @@ const EbayToggleButtonGroup: React.FC<ToggleButtonGroupProps> = ({
     ...rest
 }) => {
     const buttons = filterByType(children, EbayToggleButton)
-    const [pressedButtons, setPressedButtons] = useState({})
+    const getInitialPressedButtons = () =>
+        buttons.reduce((acc: Record<number, boolean>, button, i) => {
+            acc[i] = button.props.pressed || false
+            return acc
+        }, {})
+
+    const [pressedButtons, setPressedButtons] = useState<
+        Record<number, boolean>
+    >(getInitialPressedButtons)
+
     const handleToggle = useCallback(
-        (toggleEvent, index: number) => {
-            setPressedButtons((prevState) => {
-                let newState = {}
+        (toggleEvent: ToggleButtonEvent, index: number) => {
+            setPressedButtons((prevState: Record<number, boolean>) => {
+                let newState: Record<number, boolean> = {}
 
                 switch (variant) {
                     case 'checkbox':
@@ -51,21 +54,14 @@ const EbayToggleButtonGroup: React.FC<ToggleButtonGroupProps> = ({
                     default:
                         break
                 }
-                onChange(toggleEvent, newState)
+                if (onChange) {
+                    onChange(toggleEvent, newState)
+                }
                 return newState
             })
         },
         [variant, onChange]
     )
-
-    useEffect(() => {
-        setPressedButtons(
-            buttons.reduce((acc, button, i) => {
-                acc[i] = button.props.pressed || false
-                return acc
-            }, {})
-        )
-    }, [])
 
     return (
         <div
@@ -78,7 +74,7 @@ const EbayToggleButtonGroup: React.FC<ToggleButtonGroupProps> = ({
             {...rest}
         >
             <ul aria-label={a11yText} aria-labelledby={a11yLabelId}>
-                {buttons.map((button: ReactElement, i) => {
+                {buttons.map((button: ReactElement, i: number) => {
                     const { ...buttonRest }: ToggleButtonProps = button.props
 
                     return (
