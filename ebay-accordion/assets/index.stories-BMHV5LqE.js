32c0ae21aa7823a87b8d36017f5fa07f17d0921e
darkwebdev <darkwebdev@users.noreply.github.com>
2025-01-17 16:14:25 +0000

    Deploying to gh-pages from @ eBay/ebayui-core-react@8c877721bab4daa51d91b3f7101828112c16af3d 🚀

---
+++ ebay-accordion/assets/index.stories-BMHV5LqE.js
@@ -0,0 +1,14 @@
+import{j as i,F as p,a as r}from"./index-Zi3BSDNR.js";import{E as a}from"./progress-spinner-CT6ivDrS.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./icon-CT13wuBf.js";import"./array.polyfill.flat-BXkfvVgt.js";const c={title:"progress/ebay-progress-spinner"},e={render:()=>i(p,{children:[r("p",{children:r(a,{})}),r("p",{children:r(a,{size:"small","aria-label":"Stand by..."})}),r("p",{children:r(a,{size:"large","aria-label":"Stand by..."})})]}),name:"Default, Small & Large"};var n,s,l;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  render: () => <>
+            <p>
+                <EbayProgressSpinner />
+            </p>
+            <p>
+                <EbayProgressSpinner size="small" aria-label="Stand by..." />
+            </p>
+            <p>
+                <EbayProgressSpinner size="large" aria-label="Stand by..." />
+            </p>
+        </>,
+  name: 'Default, Small & Large'
+}`,...(l=(s=e.parameters)==null?void 0:s.docs)==null?void 0:l.source}}};const S=["DefaultSmallLarge"];export{e as DefaultSmallLarge,S as __namedExportsOrder,c as default};
---
+++ ebay-accordion/assets/index.stories-Bw2gxOa0.js
@@ -0,0 +1,125 @@
+import{a as e,F as o,j as n}from"./index-Zi3BSDNR.js";import{a as m}from"./chunk-AY7I2SME-DUSEA8To.js";import{E as re}from"./icon-CT13wuBf.js";import{E as r,a as t,b as ae,c as M}from"./menu-button-label-3dSbglul.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./array.polyfill.flat-BXkfvVgt.js";import"./utils-BsWHUBpV.js";import"./index-Bq9rzhGn.js";import"./button-B8_mvXeE.js";import"./progress-spinner-CT6ivDrS.js";import"./icon-button-4cWiY38y.js";import"./badge-OaR5SFvt.js";import"./menu-item-separator-CFup86Bd.js";const xe={component:r,title:"buttons/ebay-fake-menu-button"},h=()=>e(o,{children:n(r,{text:"eBay Menu",onExpand:()=>m("onExpand")(),onCollapse:()=>m("onCollapse")(),onKeyDown:a=>m("onKeyDown")(a),onMouseDown:(a,x)=>m("onMouseDown")(a,x),onSelect:(a,x)=>m("onSelect")(a,x),children:[e(t,{href:"http://ebay.com",onClick:a=>a.preventDefault(),children:"eBay US"}),e(t,{href:"http://ebay.de",onClick:a=>a.preventDefault(),children:"eBay DE"}),e(t,{href:"http://ebay.co.uk",onClick:a=>a.preventDefault(),children:"eBay UK"})]})}),i=()=>e(o,{children:n(r,{expanded:!0,text:"eBay Menu",children:[e(t,{href:"http://ebay.com",children:"item 1 that has very long text"}),e(t,{href:"http://ebay.de",children:"item 2"}),e(t,{href:"http://ebay.co.uk",children:"item 3"})]})}),c=()=>e(o,{children:n(r,{text:"eBay Menu",disabled:!0,children:[e(t,{href:"http://ebay.com",children:"item 1 that has very long text"}),e(t,{href:"http://ebay.com",children:"item 2"}),e(t,{href:"http://ebay.com",children:"item 3"})]})}),s=()=>e(o,{children:n(r,{text:"Settings",children:[e(re,{name:"settings16"}),e(t,{href:"http://ebay.com",children:"item 1 that has very long text"}),e(t,{href:"http://ebay.com",children:"item 2"}),e(t,{href:"http://ebay.com",children:"item 3"})]})}),p=()=>e(o,{children:n(r,{noToggleIcon:!0,text:"Menu",children:[e(t,{href:"http://ebay.com",children:"item 1 that has very long text"}),e(t,{href:"http://ebay.com",children:"item 2"}),e(t,{href:"http://ebay.com",children:"item 3"})]})}),u=()=>n(o,{children:[e("h3",{children:"Button"}),n(r,{variant:"button",text:"Button",a11yText:"Menu",children:[e(t,{href:"http://ebay.com",children:"item 1"}),e(t,{href:"http://ebay.com",children:"item 2"}),e(t,{href:"http://ebay.com",children:"item 3"})]}),e("h3",{children:"Form"}),n(r,{variant:"form",text:"Form",a11yText:"Menu inside the form",children:[e(t,{href:"http://ebay.com",children:"item 1"}),e(t,{href:"http://ebay.com",children:"item 2"}),e(t,{href:"http://ebay.com",children:"item 3"})]}),e("h3",{children:"Overflow"}),n(r,{variant:"overflow",a11yText:"Menu",children:[e(t,{href:"http://ebay.com",children:"item 1"}),e(t,{href:"http://ebay.com",children:"item 2"}),e(t,{href:"http://ebay.com",children:"item 3"})]})]}),y=()=>n(o,{children:[n(r,{variant:"button",priority:"primary",text:"Primary",a11yText:"Menu",children:[e(t,{href:"http://ebay.com",children:"item 1"}),e(t,{href:"http://ebay.com",children:"item 2"}),e(t,{href:"http://ebay.com",children:"item 3"})]}),n(r,{variant:"button",priority:"tertiary",text:"Tertiary",a11yText:"Menu",children:[e(t,{href:"http://ebay.com",children:"item 1"}),e(t,{href:"http://ebay.com",children:"item 2"}),e(t,{href:"http://ebay.com",children:"item 3"})]})]}),l=()=>e(o,{children:n(r,{text:"eBay Menu without borders!",borderless:!0,children:[e(t,{href:"http://ebay.com",children:"item 1"}),e(t,{href:"http://ebay.com",children:"item 2"}),e(t,{href:"http://ebay.com",children:"item 3"})]})}),d=()=>e(o,{children:n(r,{children:[n(ae,{children:[e("span",{style:{background:"url(https://ir.ebaystatic.com/pictures/aw/pics/cmp/ds3/sprds3_21.png)",display:"inline-block",height:"20px",marginRight:"8px",verticalAlign:"middle",width:"26px"}})," ","Fun with flags!"]}),e(t,{href:"http://ebay.com",children:"item 1"}),e(t,{href:"http://ebay.com",children:"item 2"}),e(t,{href:"http://ebay.com",children:"item 3"})]})}),b=a=>e(o,{children:n(r,{...a,text:"Complex menu",onExpand:m("Menu expanded!"),onCollapse:m("Menu collapsed!"),children:[e(t,{href:"http://ebay.com",children:"Link 1"}),e(t,{href:"http://ebay.com",current:!0,children:"Current link"}),e(M,{}),e(t,{disabled:!0,children:"Link 3 (disabled)"}),e(t,{href:"http://ebay.com",children:"Link 4"}),e(t,{href:"http://ebay.com",children:"Link 5"}),e(M,{}),e(t,{href:"http://ebay.com",onClick:m("Open login popup!"),children:"Log in"})]})}),f=a=>e(o,{children:n(r,{...a,text:"Menu has a button width",fixWidth:!0,children:[e(t,{href:"http://ebay.com",children:"item 1 that has very very long text"}),e(t,{href:"http://ebay.com",children:"item 2"}),e(t,{href:"http://ebay.com",children:"item 3"})]})}),I={render:()=>e("div",{style:{marginLeft:"100px"},children:n(r,{text:"Menu grows to the left",reverse:!0,children:[e(t,{href:"http://ebay.com",children:"item 1 that has very very long text"}),e(t,{href:"http://ebay.com",children:"item 2"}),e(t,{href:"http://ebay.com",children:"item 3"})]})}),name:"Reverse (Menu grows to the left)"};var g,k,B;h.parameters={...h.parameters,docs:{...(g=h.parameters)==null?void 0:g.docs,source:{originalSource:`() => <>
+        <EbayFakeMenuButton text="eBay Menu" onExpand={() => action('onExpand')()} onCollapse={() => action('onCollapse')()} onKeyDown={e => action('onKeyDown')(e)} onMouseDown={(e, props) => action('onMouseDown')(e, props)} onSelect={(e, props) => action('onSelect')(e, props)}>
+            <Item href="http://ebay.com" onClick={e => e.preventDefault()}>
+                eBay US
+            </Item>
+            <Item href="http://ebay.de" onClick={e => e.preventDefault()}>
+                eBay DE
+            </Item>
+            <Item href="http://ebay.co.uk" onClick={e => e.preventDefault()}>
+                eBay UK
+            </Item>
+        </EbayFakeMenuButton>
+    </>`,...(B=(k=h.parameters)==null?void 0:k.docs)==null?void 0:B.source}}};var E,v,F;i.parameters={...i.parameters,docs:{...(E=i.parameters)==null?void 0:E.docs,source:{originalSource:`() => <>
+        <EbayFakeMenuButton expanded text="eBay Menu">
+            <Item href="http://ebay.com">item 1 that has very long text</Item>
+            <Item href="http://ebay.de">item 2</Item>
+            <Item href="http://ebay.co.uk">item 3</Item>
+        </EbayFakeMenuButton>
+    </>`,...(F=(v=i.parameters)==null?void 0:v.docs)==null?void 0:F.source}}};var w,S,C;c.parameters={...c.parameters,docs:{...(w=c.parameters)==null?void 0:w.docs,source:{originalSource:`() => <>
+        <EbayFakeMenuButton text="eBay Menu" disabled>
+            <Item href="http://ebay.com">item 1 that has very long text</Item>
+            <Item href="http://ebay.com">item 2</Item>
+            <Item href="http://ebay.com">item 3</Item>
+        </EbayFakeMenuButton>
+    </>`,...(C=(S=c.parameters)==null?void 0:S.docs)==null?void 0:C.source}}};var D,T,L;s.parameters={...s.parameters,docs:{...(D=s.parameters)==null?void 0:D.docs,source:{originalSource:`() => <>
+        <EbayFakeMenuButton text="Settings">
+            <EbayIcon name="settings16" />
+            <Item href="http://ebay.com">item 1 that has very long text</Item>
+            <Item href="http://ebay.com">item 2</Item>
+            <Item href="http://ebay.com">item 3</Item>
+        </EbayFakeMenuButton>
+    </>`,...(L=(T=s.parameters)==null?void 0:T.docs)==null?void 0:L.source}}};var W,K,R;p.parameters={...p.parameters,docs:{...(W=p.parameters)==null?void 0:W.docs,source:{originalSource:`() => <>
+        <EbayFakeMenuButton noToggleIcon text="Menu">
+            <Item href="http://ebay.com">item 1 that has very long text</Item>
+            <Item href="http://ebay.com">item 2</Item>
+            <Item href="http://ebay.com">item 3</Item>
+        </EbayFakeMenuButton>
+    </>`,...(R=(K=p.parameters)==null?void 0:K.docs)==null?void 0:R.source}}};var O,P,U;u.parameters={...u.parameters,docs:{...(O=u.parameters)==null?void 0:O.docs,source:{originalSource:`() => <>
+        <h3>Button</h3>
+        <EbayFakeMenuButton variant="button" text="Button" a11yText="Menu">
+            <Item href="http://ebay.com">item 1</Item>
+            <Item href="http://ebay.com">item 2</Item>
+            <Item href="http://ebay.com">item 3</Item>
+        </EbayFakeMenuButton>
+
+        <h3>Form</h3>
+        <EbayFakeMenuButton variant="form" text="Form" a11yText="Menu inside the form">
+            <Item href="http://ebay.com">item 1</Item>
+            <Item href="http://ebay.com">item 2</Item>
+            <Item href="http://ebay.com">item 3</Item>
+        </EbayFakeMenuButton>
+
+        <h3>Overflow</h3>
+        <EbayFakeMenuButton variant="overflow" a11yText="Menu">
+            <Item href="http://ebay.com">item 1</Item>
+            <Item href="http://ebay.com">item 2</Item>
+            <Item href="http://ebay.com">item 3</Item>
+        </EbayFakeMenuButton>
+    </>`,...(U=(P=u.parameters)==null?void 0:P.docs)==null?void 0:U.source}}};var _,j,A;y.parameters={...y.parameters,docs:{...(_=y.parameters)==null?void 0:_.docs,source:{originalSource:`() => <>
+        <EbayFakeMenuButton variant="button" priority="primary" text="Primary" a11yText="Menu">
+            <Item href="http://ebay.com">item 1</Item>
+            <Item href="http://ebay.com">item 2</Item>
+            <Item href="http://ebay.com">item 3</Item>
+        </EbayFakeMenuButton>
+        <EbayFakeMenuButton variant="button" priority="tertiary" text="Tertiary" a11yText="Menu">
+            <Item href="http://ebay.com">item 1</Item>
+            <Item href="http://ebay.com">item 2</Item>
+            <Item href="http://ebay.com">item 3</Item>
+        </EbayFakeMenuButton>
+    </>`,...(A=(j=y.parameters)==null?void 0:j.docs)==null?void 0:A.source}}};var G,V,q;l.parameters={...l.parameters,docs:{...(G=l.parameters)==null?void 0:G.docs,source:{originalSource:`() => <>
+        <EbayFakeMenuButton text="eBay Menu without borders!" borderless>
+            <Item href="http://ebay.com">item 1</Item>
+            <Item href="http://ebay.com">item 2</Item>
+            <Item href="http://ebay.com">item 3</Item>
+        </EbayFakeMenuButton>
+    </>`,...(q=(V=l.parameters)==null?void 0:V.docs)==null?void 0:q.source}}};var z,H,J;d.parameters={...d.parameters,docs:{...(z=d.parameters)==null?void 0:z.docs,source:{originalSource:`() => <>
+        <EbayFakeMenuButton>
+            <EbayFakeMenuButtonLabel>
+                <span style={{
+        background: 'url(https://ir.ebaystatic.com/pictures/aw/pics/cmp/ds3/sprds3_21.png)',
+        display: 'inline-block',
+        height: '20px',
+        marginRight: '8px',
+        verticalAlign: 'middle',
+        width: '26px'
+      }} />{' '}
+                Fun with flags!
+            </EbayFakeMenuButtonLabel>
+            <Item href="http://ebay.com">item 1</Item>
+            <Item href="http://ebay.com">item 2</Item>
+            <Item href="http://ebay.com">item 3</Item>
+        </EbayFakeMenuButton>
+    </>`,...(J=(H=d.parameters)==null?void 0:H.docs)==null?void 0:J.source}}};var N,Q,X;b.parameters={...b.parameters,docs:{...(N=b.parameters)==null?void 0:N.docs,source:{originalSource:`args => <>
+        <EbayFakeMenuButton {...args} text="Complex menu" onExpand={action('Menu expanded!')} onCollapse={action('Menu collapsed!')}>
+            <Item href="http://ebay.com">Link 1</Item>
+            <Item href="http://ebay.com" current>
+                Current link
+            </Item>
+            <Separator />
+            <Item disabled>Link 3 (disabled)</Item>
+            <Item href="http://ebay.com">Link 4</Item>
+            <Item href="http://ebay.com">Link 5</Item>
+            <Separator />
+            <Item href="http://ebay.com" onClick={action('Open login popup!')}>
+                Log in
+            </Item>
+        </EbayFakeMenuButton>
+    </>`,...(X=(Q=b.parameters)==null?void 0:Q.docs)==null?void 0:X.source}}};var Y,Z,$;f.parameters={...f.parameters,docs:{...(Y=f.parameters)==null?void 0:Y.docs,source:{originalSource:`args => <>
+        <EbayFakeMenuButton {...args} text="Menu has a button width" fixWidth>
+            <Item href="http://ebay.com">item 1 that has very very long text</Item>
+            <Item href="http://ebay.com">item 2</Item>
+            <Item href="http://ebay.com">item 3</Item>
+        </EbayFakeMenuButton>
+    </>`,...($=(Z=f.parameters)==null?void 0:Z.docs)==null?void 0:$.source}}};var ee,te,ne;I.parameters={...I.parameters,docs:{...(ee=I.parameters)==null?void 0:ee.docs,source:{originalSource:`{
+  render: () => <div style={{
+    marginLeft: '100px'
+  }}>
+            <EbayFakeMenuButton text="Menu grows to the left" reverse>
+                <Item href="http://ebay.com">item 1 that has very very long text</Item>
+                <Item href="http://ebay.com">item 2</Item>
+                <Item href="http://ebay.com">item 3</Item>
+            </EbayFakeMenuButton>
+        </div>,
+  name: 'Reverse (Menu grows to the left)'
+}`,...(ne=(te=I.parameters)==null?void 0:te.docs)==null?void 0:ne.source}}};const Me=["Default","Expanded","Disabled","WithIcon","WithoutToggleIcon","Variants","Priorities","Borderless","WithCustomLabel","WithSeparator","FixedWidth","ReverseMenuGrowsToTheLeft"];export{l as Borderless,h as Default,c as Disabled,i as Expanded,f as FixedWidth,y as Priorities,I as ReverseMenuGrowsToTheLeft,u as Variants,d as WithCustomLabel,s as WithIcon,b as WithSeparator,p as WithoutToggleIcon,Me as __namedExportsOrder,xe as default};
