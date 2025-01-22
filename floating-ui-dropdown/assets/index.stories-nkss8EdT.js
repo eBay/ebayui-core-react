6cb31992fc51cdbfee58d92b4fe292b4fdbef5f9
darkwebdev <darkwebdev@users.noreply.github.com>
2025-01-22 09:28:45 +0000

    Deploying to gh-pages from @ eBay/ebayui-core-react@fefff9f01fa4f8d9afddc350fd6c276959984f96 🚀

---
+++ floating-ui-dropdown/assets/index.stories-nkss8EdT.js
@@ -0,0 +1,139 @@
+import{a as e,j as a,F as s}from"./index-Zi3BSDNR.js";import{r as t}from"./index-CBqU2yxZ.js";import{E as o}from"./button-DLWXNO0W.js";import{a as u}from"./chunk-AY7I2SME-DUSEA8To.js";import{E as J,a as G}from"./dialog-previous-button-yN_di218.js";import{E as r}from"./drawer-Qo-qdMl1.js";import"./_commonjsHelpers-BosuxZz1.js";import"./array.polyfill.flat-BKYEkrVF.js";import"./icon-CfQ0GaC3.js";import"./progress-spinner-C6Ufq_wa.js";import"./icon-button-BLh3Fq19.js";import"./badge-OaR5SFvt.js";const oe={component:r,title:"dialogs/ebay-drawer-dialog"},w=Array(100).fill(1).map((n,i)=>e("p",{children:i+1},i)),c=()=>{const[n,i]=t.useState(!1);return a(s,{children:[e(o,{onClick:()=>i(!n),children:"Open Drawer"}),a(r,{open:n,onOpen:u("onOpen"),onClose:()=>{u("onClose")(),i(!1)},onExpanded:u("onExpanded"),onCollapsed:u("onCollapsed"),a11yCloseText:"Close",a11yMaximizeText:"Maximize",a11yMinimizeText:"Minimize",children:[e("p",{children:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}),e("p",{children:e("a",{href:"http://www.ebay.com",children:"www.ebay.com"})})]})]})},d=()=>e(s,{children:a(r,{open:!0,onClose:u("Close button clicked."),a11yCloseText:"Close drawer",a11yMaximizeText:"Maximize",a11yMinimizeText:"Minimize",children:[e("p",{children:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}),e("p",{children:e("a",{href:"http://www.ebay.com",children:"www.ebay.com"})})]})}),m=()=>{const[n,i]=t.useState(!1);return a(s,{children:[e(o,{onClick:()=>i(!n),children:"Open Drawer"}),a(r,{noHandle:!0,open:n,onClose:()=>i(!1),a11yMaximizeText:"Maximize",a11yMinimizeText:"Minimize",a11yCloseText:"Close",children:[e("p",{children:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}),e("p",{children:e("a",{href:"http://www.ebay.com",children:"www.ebay.com"})})]})]})},p=()=>{const[n,i]=t.useState(!1);return a(s,{children:[e(o,{onClick:()=>i(!n),children:"Open Drawer"}),a(r,{noHandle:!0,buttonPosition:"hidden",open:n,onClose:()=>i(!1),children:[e("p",{children:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}),e("p",{children:e("a",{href:"http://www.ebay.com",children:"www.ebay.com"})})]})]})},x=()=>{const[n,i]=t.useState(!1);return a(s,{children:[e(o,{onClick:()=>i(!n),children:"Open Drawer"}),w,a(r,{open:n,onClose:()=>i(!1),a11yMaximizeText:"Maximize",a11yMinimizeText:"Minimize",a11yCloseText:"Close",children:[e(J,{children:"Scrollable content"}),w,e(G,{children:e(o,{onClick:()=>i(!1),children:"Close"})})]})]})},y=()=>{const[n,i]=t.useState(!1),l=t.createRef();return a(s,{children:[e(o,{onClick:()=>i(!n),children:"Open Drawer"}),a(r,{open:n,focus:l,onClose:()=>i(!1),a11yMaximizeText:"Maximize",a11yMinimizeText:"Minimize",a11yCloseText:"Close",children:[e("p",{children:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}),e("p",{children:e("a",{href:"http://www.ebay.com",children:"www.ebay.com"})}),e(G,{children:e(o,{ref:l,onClick:()=>i(!1),children:"Close"})})]})]})},b=()=>{const[n,i]=t.useState(!1);return a(s,{children:[e(o,{onClick:()=>i(!n),children:"Open Drawer"}),a(r,{open:n,onClose:()=>i(!1),animated:!1,a11yMaximizeText:"Maximize",a11yMinimizeText:"Minimize",a11yCloseText:"Close",children:[e("p",{children:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}),e("p",{children:e("a",{href:"http://www.ebay.com",children:"www.ebay.com"})})]})]})},f=()=>{const[n,i]=t.useState(!1),[l,I]=t.useState(!1);return a(s,{children:[e(o,{onClick:()=>i(!n),children:"Open Drawer"}),a(r,{open:n,onClose:()=>i(!1),expanded:l,a11yMaximizeText:"Maximize",a11yMinimizeText:"Minimize",a11yCloseText:"Close",children:[e("p",{children:"Trigger Dialog Expanded programmatically."}),e(o,{onClick:()=>I(!l),priority:"secondary",children:l?"Collapse Drawer":"Expand Drawer"})]})]})};var h,g,C;c.parameters={...c.parameters,docs:{...(h=c.parameters)==null?void 0:h.docs,source:{originalSource:`() => {
+  const [open, setOpen] = useState(false);
+  return <>
+            <EbayButton onClick={() => setOpen(!open)}>Open Drawer</EbayButton>
+            <EbayDrawerDialog open={open} onOpen={action('onOpen')} onClose={() => {
+      action('onClose')();
+      setOpen(false);
+    }} onExpanded={action('onExpanded')} onCollapsed={action('onCollapsed')} a11yCloseText="Close" a11yMaximizeText="Maximize" a11yMinimizeText="Minimize">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                    ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+                    sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+                    est laborum.
+                </p>
+                <p>
+                    <a href="http://www.ebay.com">www.ebay.com</a>
+                </p>
+            </EbayDrawerDialog>
+        </>;
+}`,...(C=(g=c.parameters)==null?void 0:g.docs)==null?void 0:C.source}}};var D,E,q;d.parameters={...d.parameters,docs:{...(D=d.parameters)==null?void 0:D.docs,source:{originalSource:`() => <>
+        <EbayDrawerDialog open onClose={action('Close button clicked.')} a11yCloseText="Close drawer" a11yMaximizeText="Maximize" a11yMinimizeText="Minimize">
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <p>
+                <a href="http://www.ebay.com">www.ebay.com</a>
+            </p>
+        </EbayDrawerDialog>
+    </>`,...(q=(E=d.parameters)==null?void 0:E.docs)==null?void 0:q.source}}};var z,M,O;m.parameters={...m.parameters,docs:{...(z=m.parameters)==null?void 0:z.docs,source:{originalSource:`() => {
+  const [open, setOpen] = useState(false);
+  return <>
+            <EbayButton onClick={() => setOpen(!open)}>Open Drawer</EbayButton>
+            <EbayDrawerDialog noHandle open={open} onClose={() => setOpen(false)} a11yMaximizeText="Maximize" a11yMinimizeText="Minimize" a11yCloseText="Close">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                    ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+                    sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+                    est laborum.
+                </p>
+                <p>
+                    <a href="http://www.ebay.com">www.ebay.com</a>
+                </p>
+            </EbayDrawerDialog>
+        </>;
+}`,...(O=(M=m.parameters)==null?void 0:M.docs)==null?void 0:O.source}}};var T,v,S;p.parameters={...p.parameters,docs:{...(T=p.parameters)==null?void 0:T.docs,source:{originalSource:`() => {
+  const [open, setOpen] = useState(false);
+  return <>
+            <EbayButton onClick={() => setOpen(!open)}>Open Drawer</EbayButton>
+            <EbayDrawerDialog noHandle buttonPosition="hidden" open={open} onClose={() => setOpen(false)}>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                    ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+                    sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+                    est laborum.
+                </p>
+                <p>
+                    <a href="http://www.ebay.com">www.ebay.com</a>
+                </p>
+            </EbayDrawerDialog>
+        </>;
+}`,...(S=(v=p.parameters)==null?void 0:v.docs)==null?void 0:S.source}}};var B,k,_;x.parameters={...x.parameters,docs:{...(B=x.parameters)==null?void 0:B.docs,source:{originalSource:`() => {
+  const [open, setOpen] = useState(false);
+  return <>
+            <EbayButton onClick={() => setOpen(!open)}>Open Drawer</EbayButton>
+            {numbers}
+            <EbayDrawerDialog open={open} onClose={() => setOpen(false)} a11yMaximizeText="Maximize" a11yMinimizeText="Minimize" a11yCloseText="Close">
+                <EbayDialogHeader>Scrollable content</EbayDialogHeader>
+                {numbers}
+                <EbayDialogFooter>
+                    <EbayButton onClick={() => setOpen(false)}>Close</EbayButton>
+                </EbayDialogFooter>
+            </EbayDrawerDialog>
+        </>;
+}`,...(_=(k=x.parameters)==null?void 0:k.docs)==null?void 0:_.source}}};var L,U,H;y.parameters={...y.parameters,docs:{...(L=y.parameters)==null?void 0:L.docs,source:{originalSource:`() => {
+  const [open, setOpen] = useState(false);
+  const focusRef = (createRef() as any);
+  return <>
+            <EbayButton onClick={() => setOpen(!open)}>Open Drawer</EbayButton>
+            <EbayDrawerDialog open={open} focus={focusRef} onClose={() => setOpen(false)} a11yMaximizeText="Maximize" a11yMinimizeText="Minimize" a11yCloseText="Close">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                    ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+                    sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+                    est laborum.
+                </p>
+                <p>
+                    <a href="http://www.ebay.com">www.ebay.com</a>
+                </p>
+                <EbayDialogFooter>
+                    <EbayButton ref={focusRef} onClick={() => setOpen(false)}>
+                        Close
+                    </EbayButton>
+                </EbayDialogFooter>
+            </EbayDrawerDialog>
+        </>;
+}`,...(H=(U=y.parameters)==null?void 0:U.docs)==null?void 0:H.source}}};var F,R,W;b.parameters={...b.parameters,docs:{...(F=b.parameters)==null?void 0:F.docs,source:{originalSource:`() => {
+  const [open, setOpen] = useState(false);
+  return <>
+            <EbayButton onClick={() => setOpen(!open)}>Open Drawer</EbayButton>
+            <EbayDrawerDialog open={open} onClose={() => setOpen(false)} animated={false} a11yMaximizeText="Maximize" a11yMinimizeText="Minimize" a11yCloseText="Close">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                    ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+                    sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+                    est laborum.
+                </p>
+                <p>
+                    <a href="http://www.ebay.com">www.ebay.com</a>
+                </p>
+            </EbayDrawerDialog>
+        </>;
+}`,...(W=(R=b.parameters)==null?void 0:R.docs)==null?void 0:W.source}}};var A,j,P;f.parameters={...f.parameters,docs:{...(A=f.parameters)==null?void 0:A.docs,source:{originalSource:`() => {
+  const [open, setOpen] = useState(false);
+  const [expanded, setExpanded] = useState(false);
+  return <>
+            <EbayButton onClick={() => setOpen(!open)}>Open Drawer</EbayButton>
+            <EbayDrawerDialog open={open} onClose={() => setOpen(false)} expanded={expanded} a11yMaximizeText="Maximize" a11yMinimizeText="Minimize" a11yCloseText="Close">
+                <p>Trigger Dialog Expanded programmatically.</p>
+
+                <EbayButton onClick={() => setExpanded(!expanded)} priority="secondary">
+                    {expanded ? 'Collapse Drawer' : 'Expand Drawer'}
+                </EbayButton>
+            </EbayDrawerDialog>
+        </>;
+}`,...(P=(j=f.parameters)==null?void 0:j.docs)==null?void 0:P.source}}};const te=["_Default","_Opened","_WithoutHandle","_WithoutHandleAndCloseButton","_LotsOfContent","_CustomFocus","_WithoutAnimation","_TriggerExpanded"];export{y as _CustomFocus,c as _Default,x as _LotsOfContent,d as _Opened,f as _TriggerExpanded,b as _WithoutAnimation,m as _WithoutHandle,p as _WithoutHandleAndCloseButton,te as __namedExportsOrder,oe as default};
