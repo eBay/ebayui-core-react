e6163d3063f1a7bd36a16dd6ff94667397ce9ebb
Aaron Nance <anance@ebay.com>
2025-02-04 09:47:14 -0800

    feat: Add skeleton loading component (#378)
    * 308: Add skeleton loading component
    
    * fix eslint
    
    * Add support for 'as' for components and 'color' - add changeset
    
    * Add readme content
    
    * Remove default aria-label
    
    ---------
    
    Co-authored-by: HenriqueLimas <henrique.ramos.limas@gmail.com>

---
+++ src/ebay-skeleton/index.ts
@@ -0,0 +1,6 @@
+export { default as EbaySkeleton, type EbaySkeletonProps } from './ebay-skeleton'
+export { default as EbaySkeletonAvatar, type EbaySkeletonAvatarProps } from './ebay-skeleton-avatar'
+export { default as EbaySkeletonButton, type EbaySkeletonButtonProps } from './ebay-skeleton-button'
+export { default as EbaySkeletonImage, type EbaySkeletonImageProps } from './ebay-skeleton-image'
+export { default as EbaySkeletonText, type EbaySkeletonTextProps } from './ebay-skeleton-text'
+export { default as EbaySkeletonTextbox, type EbaySkeletonTextboxProps } from './ebay-skeleton-textbox'
