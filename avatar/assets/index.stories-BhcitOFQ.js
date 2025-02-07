79b3a9c1f51103fbb0f1dbe00e83ca5e7094cdca
HenriqueLimas <HenriqueLimas@users.noreply.github.com>
2025-02-07 01:59:24 +0000

    Deploying to gh-pages from @ eBay/ebayui-core-react@f2f006661ebf2a93a684769c18d2fac3ab91bbb0 🚀

---
+++ avatar/assets/index.stories-BhcitOFQ.js
@@ -0,0 +1,42 @@
+import{E as x}from"./calendar-DJewmXIk.js";import"./index-Zi3BSDNR.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./icon-button-B6KyUsdh.js";import"./icon-C975DJ98.js";import"./array.polyfill.flat-C_pnjiEO.js";import"./badge-OaR5SFvt.js";const w={component:x,title:"building blocks/ebay-calendar"},e={},r={args:{interactive:!0,selected:"2024-01-03"}},a={args:{interactive:!0,numMonths:2,selected:"2024-01-03"}},t={args:{interactive:!0,navigable:!0,numMonths:2,selected:"2024-01-03"}},s={args:{interactive:!0,range:!0,numMonths:2,selected:["2024-01-03","2025-02-01"]}},n={args:{interactive:!0,navigable:!0,range:!0,numMonths:2,selected:["2024-01-03","2025-02-01"],disableBefore:"2024-01-03",disableAfter:"2025-02-01"}},o={args:{interactive:!0,numMonths:2,disableWeekdays:[0,6]}};var c,i,u;e.parameters={...e.parameters,docs:{...(c=e.parameters)==null?void 0:c.docs,source:{originalSource:"{}",...(u=(i=e.parameters)==null?void 0:i.docs)==null?void 0:u.source}}};var d,m,l;r.parameters={...r.parameters,docs:{...(d=r.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    interactive: true,
+    selected: '2024-01-03'
+  }
+}`,...(l=(m=r.parameters)==null?void 0:m.docs)==null?void 0:l.source}}};var p,g,b;a.parameters={...a.parameters,docs:{...(p=a.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    interactive: true,
+    numMonths: 2,
+    selected: '2024-01-03'
+  }
+}`,...(b=(g=a.parameters)==null?void 0:g.docs)==null?void 0:b.source}}};var v,M,f;t.parameters={...t.parameters,docs:{...(v=t.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  args: {
+    interactive: true,
+    navigable: true,
+    numMonths: 2,
+    selected: '2024-01-03'
+  }
+}`,...(f=(M=t.parameters)==null?void 0:M.docs)==null?void 0:f.source}}};var h,S,y;s.parameters={...s.parameters,docs:{...(h=s.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  args: {
+    interactive: true,
+    range: true,
+    numMonths: 2,
+    selected: ['2024-01-03', '2025-02-01']
+  }
+}`,...(y=(S=s.parameters)==null?void 0:S.docs)==null?void 0:y.source}}};var A,D,k;n.parameters={...n.parameters,docs:{...(A=n.parameters)==null?void 0:A.docs,source:{originalSource:`{
+  args: {
+    interactive: true,
+    navigable: true,
+    range: true,
+    numMonths: 2,
+    selected: ['2024-01-03', '2025-02-01'],
+    disableBefore: '2024-01-03',
+    disableAfter: '2025-02-01'
+  }
+}`,...(k=(D=n.parameters)==null?void 0:D.docs)==null?void 0:k.source}}};var B,W,E;o.parameters={...o.parameters,docs:{...(B=o.parameters)==null?void 0:B.docs,source:{originalSource:`{
+  args: {
+    interactive: true,
+    numMonths: 2,
+    disableWeekdays: [0, 6]
+  }
+}`,...(E=(W=o.parameters)==null?void 0:W.docs)==null?void 0:E.source}}};const z=["Default","Interactive","MultipleMonths","Navigable","RangeSelected","DisableBeforeAndAfter","DisableWeekdays"];export{e as Default,n as DisableBeforeAndAfter,o as DisableWeekdays,r as Interactive,a as MultipleMonths,t as Navigable,s as RangeSelected,z as __namedExportsOrder,w as default};
