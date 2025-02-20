f556f8a3137b607c1e4e2f93335d0c74ce74f91b
HenriqueLimas <HenriqueLimas@users.noreply.github.com>
2025-02-20 02:39:20 +0000

    Deploying to gh-pages from @ eBay/ebayui-core-react@3e9fffc92ad6c935676e3bc90b2cd2ccbfc3a4b9 🚀

---
+++ table/assets/index.stories-C2vJLhqA.js
@@ -0,0 +1,35 @@
+import{a as t,F as c,j as B}from"./index-Zi3BSDNR.js";import{E as e}from"./cta-button-BX8DuD_d.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./array.polyfill.flat-ESCkjixS.js";import"./icon-f_3MOqj8.js";import"./button-Dy2rVwyP.js";import"./progress-spinner-oQq1ixmb.js";const F={component:e,title:"buttons/ebay-cta-button"},a=()=>t(c,{children:t("p",{children:t(e,{href:"https://ebay.com",children:"Take Action Now!"})})}),r=()=>t(c,{children:t("p",{children:t(e,{href:"https://ebay.com",size:"large",children:"Large Button"})})}),n=()=>B(c,{children:[t("p",{children:t(e,{href:"https://ebay.com",fluid:!0,children:"100%"})}),t("p",{children:t(e,{href:"https://ebay.com",size:"large",fluid:!0,children:"Large!"})})]}),o=()=>B("div",{style:{maxWidth:"200px"},children:[t("p",{children:t(e,{href:"https://ebay.com",truncate:!0,children:"Wide Long Call To Action!"})}),t("p",{children:t(e,{href:"https://ebay.com",size:"large",truncate:!0,children:"Go Big with Call To Action!"})})]});var s,i,p;a.parameters={...a.parameters,docs:{...(s=a.parameters)==null?void 0:s.docs,source:{originalSource:`() => <>
+        <p>
+            <EbayCtaButton href="https://ebay.com">Take Action Now!</EbayCtaButton>
+        </p>
+    </>`,...(p=(i=a.parameters)==null?void 0:i.docs)==null?void 0:p.source}}};var u,d,l;r.parameters={...r.parameters,docs:{...(u=r.parameters)==null?void 0:u.docs,source:{originalSource:`() => <>
+        <p>
+            <EbayCtaButton href="https://ebay.com" size="large">
+                Large Button
+            </EbayCtaButton>
+        </p>
+    </>`,...(l=(d=r.parameters)==null?void 0:d.docs)==null?void 0:l.source}}};var h,m,b;n.parameters={...n.parameters,docs:{...(h=n.parameters)==null?void 0:h.docs,source:{originalSource:`() => <>
+        <p>
+            <EbayCtaButton href="https://ebay.com" fluid>
+                100%
+            </EbayCtaButton>
+        </p>
+        <p>
+            <EbayCtaButton href="https://ebay.com" size="large" fluid>
+                Large!
+            </EbayCtaButton>
+        </p>
+    </>`,...(b=(m=n.parameters)==null?void 0:m.docs)==null?void 0:b.source}}};var y,f,g;o.parameters={...o.parameters,docs:{...(y=o.parameters)==null?void 0:y.docs,source:{originalSource:`() => <div style={{
+  maxWidth: '200px'
+}}>
+        <p>
+            <EbayCtaButton href="https://ebay.com" truncate>
+                Wide Long Call To Action!
+            </EbayCtaButton>
+        </p>
+        <p>
+            <EbayCtaButton href="https://ebay.com" size="large" truncate>
+                Go Big with Call To Action!
+            </EbayCtaButton>
+        </p>
+    </div>`,...(g=(f=o.parameters)==null?void 0:f.docs)==null?void 0:g.source}}};const S=["Default","Large","Fluid","Truncated"];export{a as Default,n as Fluid,r as Large,o as Truncated,S as __namedExportsOrder,F as default};
