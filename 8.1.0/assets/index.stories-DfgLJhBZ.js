7d4eb3f12290fb76bf920a39613e6d209a346570
HenriqueLimas <HenriqueLimas@users.noreply.github.com>
2025-02-25 21:00:01 +0000

    Deploying to gh-pages from @ eBay/ebayui-core-react@544ec7b52ae4d37b50af0a3c35729ee458ca58ed 🚀

---
+++ 8.1.0/assets/index.stories-DfgLJhBZ.js
@@ -0,0 +1,11 @@
+import{a as e,F as a}from"./index-Zi3BSDNR.js";import{E as r}from"./signal-BoKJSNOA.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";const D={component:r,title:"graphics & icons/ebay-signal"},t=()=>e(a,{children:e(r,{children:"Default"})}),s=()=>e(a,{children:e(r,{status:"trustworthy",children:"Trustworthy"})}),n=()=>e(a,{children:e(r,{status:"recent",children:"Recent"})}),o=()=>e(a,{children:e(r,{status:"time-sensitive",children:"Time-Sensitive"})}),i=()=>e(a,{children:e(r,{status:"neutral",children:"Neutral"})});var c,u,l;t.parameters={...t.parameters,docs:{...(c=t.parameters)==null?void 0:c.docs,source:{originalSource:`() => <>
+        <EbaySignal>Default</EbaySignal>
+    </>`,...(l=(u=t.parameters)==null?void 0:u.docs)==null?void 0:l.source}}};var m,d,p;s.parameters={...s.parameters,docs:{...(m=s.parameters)==null?void 0:m.docs,source:{originalSource:`() => <>
+        <EbaySignal status="trustworthy">Trustworthy</EbaySignal>
+    </>`,...(p=(d=s.parameters)==null?void 0:d.docs)==null?void 0:p.source}}};var S,g,y;n.parameters={...n.parameters,docs:{...(S=n.parameters)==null?void 0:S.docs,source:{originalSource:`() => <>
+        <EbaySignal status="recent">Recent</EbaySignal>
+    </>`,...(y=(g=n.parameters)==null?void 0:g.docs)==null?void 0:y.source}}};var h,E,b;o.parameters={...o.parameters,docs:{...(h=o.parameters)==null?void 0:h.docs,source:{originalSource:`() => <>
+        <EbaySignal status="time-sensitive">Time-Sensitive</EbaySignal>
+    </>`,...(b=(E=o.parameters)==null?void 0:E.docs)==null?void 0:b.source}}};var f,v,w;i.parameters={...i.parameters,docs:{...(f=i.parameters)==null?void 0:f.docs,source:{originalSource:`() => <>
+        <EbaySignal status="neutral">Neutral</EbaySignal>
+    </>`,...(w=(v=i.parameters)==null?void 0:v.docs)==null?void 0:w.source}}};const F=["defaultCase","trustworthy","recent","timeSensitive","neutral"];export{F as __namedExportsOrder,D as default,t as defaultCase,i as neutral,n as recent,o as timeSensitive,s as trustworthy};
