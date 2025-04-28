cc56974d9ee067f7370da726b8a1aec29764a460
HenriqueLimas <henrique.ramos.limas@gmail.com>
2025-04-28 19:44:49 +0000

    Deploying to gh-pages from @ eBay/ebayui-core-react@0d77345e3424dfa2ad16fbf9096ac76cde4f6061 🚀

---
+++ main/assets/random-id-W0JqM5gh.js
@@ -0,0 +1,1 @@
+import{r as t}from"./index-CBqU2yxZ.js";const o=()=>Math.random().toString(16).slice(-4),a=()=>{const[r,s]=t.useState("");return t.useEffect(()=>{s(o())},[]),r};export{o as r,a as u};
