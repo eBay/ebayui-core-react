621dc000645dfb6c354f11835dc1715e60fcb064
HenriqueLimas <henrique.ramos.limas@gmail.com>
2025-05-06 18:32:13 +0000

    Deploying to gh-pages from @ eBay/ebayui-core-react@423285a4900ac41a48a12b1e499ce442e98882c5 🚀

---
+++ main/assets/index.stories-A_NFILiL.js
@@ -0,0 +1,5 @@
+import{j as s,F as c,a as t}from"./index-Zi3BSDNR.js";import{E as r}from"./svg-Bv8Sp42a.js";import{E as n}from"./icon-DPIICv8X.js";import"./index-CBqU2yxZ.js";import"./_commonjsHelpers-BosuxZz1.js";import"./array.polyfill.flat-Ce2qUowv.js";import"./random-id-W0JqM5gh.js";const l={component:r,title:"graphics & icons/ebay-svg"},o=()=>s(c,{children:[t(r,{icons:["notification16","attention16"]}),t(n,{name:"notification16"}),t(n,{name:"attention16"})]});var a,i,e;o.parameters={...o.parameters,docs:{...(a=o.parameters)==null?void 0:a.docs,source:{originalSource:`() => <>
+        <EbaySvg icons={['notification16', 'attention16']} />
+        <EbayIcon name="notification16" />
+        <EbayIcon name="attention16" />
+    </>`,...(e=(i=o.parameters)==null?void 0:i.docs)==null?void 0:e.source}}};const y=["FilteredIcons"];export{o as FilteredIcons,y as __namedExportsOrder,l as default};
